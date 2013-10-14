b1da8ea562414e43347d96f0df5624d92e3d6a3d
Anthony Ikeda <aikeda@constantcontact.com>
2013-10-14 08:34:46 -0700

    More progress understanding angular.js
    Updated the model slightly, added the unit tests for TDD approach.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,5 +8,14 @@ phonecatApp.controller('PhoneListCtrl', function PhoneListCtrl($scope) {
 		{'name':'Nexus S', 'snippet':'Fast just got faster with nexus s'},
 		{'name':'iPhone 5C', 'snippet':'Your life just got more colourful'},
 		{'name':'iPhone 5S', 'snippet':'Take mobile to the next level'}
-	]
+	];
+
+	$scope.people = [
+		{'name':'Anthony Ikeda', 'gender':'Male'},
+		{'name':'Paul Potter', 'gender':'Male'},
+		{'name':'Wendy Williams', 'gender':'Female'},
+		{'name':'Jessica Jones', 'gender':'Female'}
+	];
+
+	$scope.age = 2013 - 1974;
 });
